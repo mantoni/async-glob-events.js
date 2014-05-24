335ecc74a0f6e005128a7b4eccfa2d57c90d264e
Maximilian Antoni <mail@maxantoni.de>
2014-05-24 17:30:14 +0200

    Pass 'removeListener' events to super

---
+++ test/removeListener-test.js
@@ -0,0 +1,38 @@
+/*
+ * async-glob-events.js
+ *
+ * Copyright (c) 2014 Maximilian Antoni
+ *
+ * @license MIT
+ */
+/*global describe, it, beforeEach*/
+'use strict';
+
+var assert = require('assert');
+var AsyncEmitter = require('../lib/events').AsyncEmitter;
+
+
+function noop() { return; }
+noop(); // Coverage
+
+
+describe('removeListener', function () {
+  var e;
+
+  beforeEach(function () {
+    e = new AsyncEmitter();
+  });
+
+  it('emits removeListener event with correct function', function () {
+    var arg;
+    e.addListener('removeListener', function (event, fn) {
+      /*jslint unparam: true*/
+      arg = fn;
+    });
+
+    e.removeListener('test', noop);
+
+    assert.strictEqual(arg, noop);
+  });
+
+});
